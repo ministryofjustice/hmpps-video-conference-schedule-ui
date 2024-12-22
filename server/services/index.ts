81886eb8f5ec4ac40eb7158c9624de93fe9b0328
Stephen McVeigh <smcveigh941@gmail.com>
2024-12-22 23:01:08 +0000

    wip

--- server/services/index.ts
+++ server/services/index.ts
@@ -2,23 +2,19 @@ import { dataAccess } from '../data'
 import AuditService from './auditService'
 import UserService from './userService'
 import PrisonService from './prisonService'
-import AppointmentService from './appointmentService'
 
 export const services = () => {
-  const { applicationInfo, manageUsersApiClient, hmppsAuditClient, prisonRegisterApiClient, prisonApiClient } =
-    dataAccess()
+  const { applicationInfo, manageUsersApiClient, hmppsAuditClient, prisonRegisterApiClient } = dataAccess()
 
   const userService = new UserService(manageUsersApiClient)
   const auditService = new AuditService(hmppsAuditClient)
   const prisonService = new PrisonService(prisonRegisterApiClient)
-  const appointmentService = new AppointmentService(prisonApiClient)
 
   return {
     applicationInfo,
     userService,
     auditService,
     prisonService,
-    appointmentService,
   }
 }
 
