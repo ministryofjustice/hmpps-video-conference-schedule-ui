bed633a11ee06ddae43d562e608d80505d295ca7
Stephen McVeigh <smcveigh941@gmail.com>
2025-02-13 17:39:15 +0000

    BAVL-480: Fix filters expanding when closing the print dialogue (#46)

--- frontend/components/moj-filter/moj-filter.js
+++ frontend/components/moj-filter/moj-filter.js
@@ -2,6 +2,11 @@ function MojFilter($filter) {
   this.toggleButton = $filter.querySelector('.moj-action-bar__filter')
   this.filtersApplied = $filter.getAttribute('filters-applied') === 'true'
 
+  // The MOJFrontend component from the moj-frontend library contains a bug where the filters will pop out and expand
+  // when the media changes from `print` to `screen`, i.e. when you close the print dialogue. Overriding the following function
+  // resolves that, although this will just mean that the smaller screens (ipad, phones) may not behave as expected. Meh.
+  MOJFrontend.FilterToggleButton.prototype.setupResponsiveChecks = () => undefined;
+
   new MOJFrontend.FilterToggleButton({
     bigModeMediaQuery: '(min-width: 48.063em)',
     startHidden: !this.filtersApplied,
