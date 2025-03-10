e9eda7be39811d37c34df0812fc1c5f1975fd8aa
Stephen McVeigh <smcveigh941@gmail.com>
2025-03-10 11:49:09 +0000

    BAVL-480: Hide video URL for pre and post appointment (#54)

--- server/services/scheduleService.ts
+++ server/services/scheduleService.ts
@@ -139,6 +139,10 @@ export default class ScheduleService {
     const createdTime = bvlsAppointment?.createdTime || scheduledAppointment.createdTime
     const updatedTime = bvlsAppointment?.updatedTime || scheduledAppointment.updatedTime
     const videoLinkRequired = bvlsAppointment?.appointmentType === 'VLB_COURT_MAIN'
+    const isMainAppointmentOfBooking =
+      bvlsAppointment &&
+      bvlsAppointment?.appointmentType !== 'VLB_COURT_PRE' &&
+      bvlsAppointment?.appointmentType !== 'VLB_COURT_POST'
 
     const buildTags = () => {
       const appointmentDate = parseDate(scheduledAppointment.date)
@@ -182,7 +186,7 @@ export default class ScheduleService {
       appointmentLocationDescription: scheduledAppointment.locationDescription,
       videoBookingId: bvlsAppointment?.videoBookingId,
       videoLinkRequired,
-      videoLink: bvlsAppointment?.videoUrl,
+      videoLink: isMainAppointmentOfBooking ? bvlsAppointment.videoUrl : undefined,
       appointmentSubtypeDescription:
         (bvlsAppointment?.appointmentType === 'VLB_COURT_MAIN' && bvlsAppointment?.hearingTypeDescription) ||
         (bvlsAppointment?.appointmentType === 'VLB_PROBATION' && bvlsAppointment?.probationMeetingTypeDescription) ||
