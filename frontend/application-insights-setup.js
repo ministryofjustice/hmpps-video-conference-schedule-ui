cd1f772ccb0d7b073802643ef8be498165d18311
Stephen McVeigh <smcveigh941@gmail.com>
2025-04-07 15:11:11 +0100

    BAVL-669: Remove auto tracking of click events, as it may have recorded PII. (#61)

--- frontend/application-insights-setup.js
+++ frontend/application-insights-setup.js
@@ -1,35 +1,19 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
-import { ClickAnalyticsPlugin } from '@microsoft/applicationinsights-clickanalytics-js'
 
 if (window.applicationInsightsConnectionString) {
-  const clickPluginInstance = new ClickAnalyticsPlugin()
-  const clickPluginConfig = {
-    autoCapture: true,
-    dataTags: {
-      useDefaultContentNameOrId: true,
-    },
-  }
-  const snippet = {
+  const init = new ApplicationInsights({
     config: {
       connectionString: window.applicationInsightsConnectionString,
-      extensions: [
-        clickPluginInstance,
-      ],
-      extensionConfig: {
-        [clickPluginInstance.identifier]: clickPluginConfig,
-      },
       autoTrackPageVisitTime: true,
     },
-  }
-
-  const init = new ApplicationInsights(snippet)
+  })
   const appInsights = init.loadAppInsights()
   appInsights.addTelemetryInitializer(function (envelope) {
-    envelope.tags["ai.cloud.role"] = window.applicationInsightsApplicationName
-    envelope.tags["ai.application.ver"] = window.buildNumber
-  });
+    envelope.tags['ai.cloud.role'] = window.applicationInsightsApplicationName
+    envelope.tags['ai.application.ver'] = window.buildNumber
+  })
   appInsights.setAuthenticatedUserContext(window.authenticatedUser)
-  appInsights.trackPageView();
+  appInsights.trackPageView()
 
   initMetricClickEvents(appInsights)
 }
