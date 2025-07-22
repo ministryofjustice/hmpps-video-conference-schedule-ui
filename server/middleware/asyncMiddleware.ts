59948b0179610852e661f51a59aac7169d5537ea
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-07-22 13:41:40 +0100

    BAVL-1005 intiial commit for migrating from Express 4 to Express 5.

--- server/middleware/asyncMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction, RequestHandler } from 'express'
-import { NotFound } from 'http-errors'
-
-export default function asyncMiddleware(fn: RequestHandler) {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    if (fn) Promise.resolve(fn(req, res, next)).catch(next)
-    else next(new NotFound())
-  }
-}
