e314e582601b6c191b41ceaff61c0dbe512e481d
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-11-28 11:47:50 +0000

    BAVL-1164 include the username in app insights logging to assist with user behaviour monitoring.

--- server/interfaces/hmppsUser.ts
+++ server/interfaces/hmppsUser.ts
@@ -1,6 +1,20 @@
 import { User } from '../@types/manageUsersApi/types'
 
-export interface HmppsUser extends User {
+/**
+ * Prison users are those that have a user account in NOMIS.
+ * HMPPS Auth automatically grants these users a `ROLE_PRISON` role.
+ * Prison users have an additional numerical staffId. The userId is
+ * a stringified version of the staffId. Some teams may need to separately
+ * retrieve the user case load (which prisons that a prison user has access
+ * to) and store it here, an example can be found in `hmpps-prisoner-profile`.
+ */
+export interface PrisonUser extends User {
+  authSource: 'nomis'
+  staffId: number
+  activeCaseLoadId?: string
+}
+
+export interface HmppsUser extends PrisonUser {
   displayName: string
   roles: string[]
 }
