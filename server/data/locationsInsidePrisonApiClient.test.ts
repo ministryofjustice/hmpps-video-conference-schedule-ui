70d54ff07acb8bc2b2316215a98d1bf305c0ad73
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-07-01 14:16:31 +0100

    MAPB-579 Fix prettier line wrapping in locations client test

--- server/data/locationsInsidePrisonApiClient.test.ts
+++ server/data/locationsInsidePrisonApiClient.test.ts
@@ -29,7 +29,9 @@ describe('locationsInsidePrisonApiClient', () => {
       const response = { data: 'data' }
 
       fakeLocationsInsidePrisonApiClient
-        .get('/locations/non-residential/prison/MDI/service/APPOINTMENT?formatLocalName=true&sortByLocalName=true&filterParents=false')
+        .get(
+          '/locations/non-residential/prison/MDI/service/APPOINTMENT?formatLocalName=true&sortByLocalName=true&filterParents=false',
+        )
         .matchHeader('authorization', `Bearer systemToken`)
         .reply(200, response)
 
