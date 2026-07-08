869665dede71e28a90e77ef95b60afa4eea13d36
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-07-08 14:39:51 +0100

    BAVL-1386 major version bump of the frontend components version. (#173)

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,9 @@
-import $ from 'jquery'
 import * as GOVUKFrontend from 'govuk-frontend'
 import * as MOJFrontend from '@ministryofjustice/frontend'
 import * as VideoConferenceScheduleFrontend from './all'
 import './application-insights-setup'
 
 //Make packages globally accessible
-window.$ = $ // JQuery required by MoJ frontend. https://design-patterns.service.justice.gov.uk/get-started/setting-up-javascript/
 window.GOVUKFrontend = GOVUKFrontend
 window.MOJFrontend = MOJFrontend
 
