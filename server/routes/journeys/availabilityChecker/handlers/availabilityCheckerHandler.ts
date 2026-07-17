34cd3637823c219cde873c027e8910c910843950
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-07-17 16:53:24 +0100

    BAVL-1359 extending the view with an improved layout but using hardcoded data (for now).

--- server/routes/journeys/availabilityChecker/handlers/availabilityCheckerHandler.ts
+++ server/routes/journeys/availabilityChecker/handlers/availabilityCheckerHandler.ts
@@ -1,10 +1,25 @@
 // eslint-disable-next-line max-classes-per-file
 import { Request, Response } from 'express'
+import { Expose, Transform } from 'class-transformer'
+import { IsNotEmpty } from 'class-validator'
+import { formatDate, isValid, startOfDay } from 'date-fns'
 import { Page } from '../../../../services/auditService'
 import { PageHandler } from '../../../interfaces/pageHandler'
 import config from '../../../../config'
+import { parseDatePickerDate } from '../../../../utils/utils'
+import IsValidDate from '../../../validators/isValidDate'
 
-class Body {}
+class Body {
+  @Expose()
+  @Transform(({ value }) => parseDatePickerDate(value))
+  @IsValidDate({ message: 'Enter a valid date' })
+  @IsNotEmpty({ message: 'Enter a date' })
+  date: Date
+
+  @Expose()
+  @IsNotEmpty({ message: 'Select a period' })
+  period: string
+}
 
 export default class AvailabilityCheckerHandler implements PageHandler {
   public PAGE_NAME = Page.AVAILABILTY_CHECKER_PAGE
@@ -16,9 +31,24 @@ export default class AvailabilityCheckerHandler implements PageHandler {
     const { user } = res.locals
 
     if (enabledPrisons && enabledPrisons.includes(user.activeCaseLoadId)) {
-      res.render('pages/availabilityChecker/availabilityChecker')
+      const prison = req.middleware!.prison!
+      const dateFromQueryParam = new Date(req.query.date?.toString())
+      const date = startOfDay(isValid(dateFromQueryParam) ? dateFromQueryParam : new Date())
+      const period = req.query.period || 'AM'
+
+      res.render('pages/availabilityChecker/availabilityChecker', { prison, date, period })
     } else {
       res.render('pages/error/404')
     }
+  }
+
+  POST = async (req: Request, res: Response) => {
+    const { date, period } = req.body
+    const queryParams = new URLSearchParams({
+      date: formatDate(date, 'yyyy-MM-dd'),
+      period,
+    })
+
+    return res.redirect(`availability-checker?${queryParams}`)
   }
 }
