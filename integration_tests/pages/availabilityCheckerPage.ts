30e370e355e8825bc462b7712ff5b2a5cbfe15a7
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-08-04 09:16:40 +0100

    BAVL-1359 small styling changes and integration test coverage for availability checker. (#194)

---
+++ integration_tests/pages/availabilityCheckerPage.ts
@@ -0,0 +1,145 @@
+import { expect, type Locator, type Page } from '@playwright/test'
+import { getDay } from 'date-fns'
+import AbstractPage from './abstractPage'
+import { formatDate } from '../../server/utils/utils'
+
+export enum WeekDay {
+  MONDAY = 'monday',
+  TUESDAY = 'tuesday',
+  WEDNESDAY = 'wednesday',
+  THURSDAY = 'thursday',
+  FRIDAY = 'friday',
+}
+
+export enum Session {
+  MORNING = 'Morning',
+  AFTERNOON = 'Afternoon',
+  EVENING = 'Evening',
+}
+
+export default class AvailabilityCheckerPage extends AbstractPage {
+  private readonly header: Locator
+
+  private readonly date: Locator
+
+  private readonly morningSession: Locator
+
+  private readonly afternoonSession: Locator
+
+  private readonly eveningSession: Locator
+
+  private readonly updateButton: Locator
+
+  private readonly previousWeekLink: Locator
+
+  private readonly nextWeekLink: Locator
+
+  private constructor(page: Page) {
+    super(page)
+    this.header = page.locator('h1', { hasText: 'Video link availability checker:  Moorland (HMP)' })
+    this.date = page.getByLabel('Date')
+    this.morningSession = page.locator('#period')
+    this.afternoonSession = page.locator('#period-2')
+    this.eveningSession = page.locator('#period-3')
+    this.updateButton = page.getByRole('button', { name: 'Update' })
+    this.previousWeekLink = page.getByRole('link', { name: 'Previous week' })
+    this.nextWeekLink = page.getByRole('link', { name: 'Next week' })
+  }
+
+  static async verifyOnPage(page: Page): Promise<AvailabilityCheckerPage> {
+    const availabilityCheckerPage = new AvailabilityCheckerPage(page)
+    await expect(availabilityCheckerPage.header).toBeVisible()
+    await availabilityCheckerPage.verifyNoAccessViolationsOnPage()
+    return availabilityCheckerPage
+  }
+
+  async selectDate(date: Date) {
+    await this.date.fill(formatDate(date, 'dd/MM/yyyy') as string)
+  }
+
+  async selectSession(session: Session) {
+    if (session === Session.MORNING) {
+      await this.morningSession.check()
+    }
+
+    if (session === Session.AFTERNOON) {
+      await this.afternoonSession.check()
+    }
+
+    if (session === Session.EVENING) {
+      await this.eveningSession.check()
+    }
+  }
+
+  async assertSessionSelected(session: Session) {
+    await expect(this.page.getByLabel(session)).toBeChecked()
+  }
+
+  async update() {
+    await this.updateButton.click()
+  }
+
+  async previousWeek() {
+    await this.previousWeekLink.click()
+  }
+
+  async nextWeek() {
+    await this.nextWeekLink.click()
+  }
+
+  async selectWeekDayTab(date: Date) {
+    const day = getDay(date)
+    expect(day).toBeGreaterThan(0)
+    expect(day).toBeLessThan(6)
+
+    let dayOfWeek: string = ''
+
+    switch (day) {
+      case 1:
+        dayOfWeek = WeekDay.MONDAY
+        break
+      case 2:
+        dayOfWeek = WeekDay.TUESDAY
+        break
+      case 3:
+        dayOfWeek = WeekDay.WEDNESDAY
+        break
+      case 4:
+        dayOfWeek = WeekDay.THURSDAY
+        break
+      case 5:
+        dayOfWeek = WeekDay.FRIDAY
+        break
+      default:
+        break
+    }
+
+    await this.page.locator(`a[id="tab_${dayOfWeek}"]`).click()
+  }
+
+  async assertWeekDayTabSelected(day: WeekDay) {
+    const expected = await this.page.locator(`a[id="tab_${day}"]`).getAttribute('aria-selected')
+
+    expect(expected).toBeTruthy()
+  }
+
+  async assertRoomAvailable(roomIdentifier: string, date: Date, hour: number) {
+    await expect(
+      this.getByDataQa(`${roomIdentifier}-available-${formatDate(date, 'yyyy-MM-dd')}-${hour}`),
+    ).toContainText('Available')
+  }
+
+  async assertRoomPartiallyAvailable(roomIdentifier: string, date: Date, startTime: string, endTime: string) {
+    await expect(
+      this.getByDataQa(`${roomIdentifier}-partial-${formatDate(date, 'yyyy-MM-dd')}-${startTime}-${endTime}`),
+    ).toContainText(`${startTime} - ${endTime}`)
+  }
+
+  async assertRoomBooked(roomIdentifier: string, date: Date, hour: number) {
+    await expect(this.getByDataQa(`${roomIdentifier}-booked-${formatDate(date, 'yyyy-MM-dd')}-${hour}`)).toContainText(
+      'Booked',
+    )
+  }
+
+  private getByDataQa = (dataQa: string) => this.page.locator(`[data-qa='${dataQa}']`)
+}
---
+++ integration_tests/specs/availabilityChecker.spec.ts
@@ -0,0 +1,155 @@
+import { test } from '@playwright/test'
+import { previousFriday, previousMonday, previousThursday, previousTuesday, previousWednesday } from 'date-fns'
+import bookAVideoLinkApi from '../mockApis/bookAVideoLinkApi'
+import componentsApi from '../mockApis/componentsApi'
+import manageUsersApi from '../mockApis/manageUsersApi'
+import { resetStubs } from '../mockApis/wiremock'
+import { login } from '../testUtils'
+import AvailabilityCheckerPage, { Session, WeekDay } from '../pages/availabilityCheckerPage'
+
+test.describe('Availability Checker', () => {
+  const monday = new Date('2026-07-27')
+  const tuesday = new Date('2026-07-28')
+  const wednesday = new Date('2026-07-29')
+  const thursday = new Date('2026-07-30')
+  const friday = new Date('2026-07-31')
+
+  test.beforeEach(async () => {
+    await bookAVideoLinkApi.stubGetPrison()
+    await bookAVideoLinkApi.stubGetCourts()
+    await bookAVideoLinkApi.stubGetProbationTeams()
+    await bookAVideoLinkApi.stubGetVideoLinkEvents({
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+    })
+    await componentsApi.stubComponents()
+    await manageUsersApi.stubUser()
+  })
+
+  test.afterEach(async () => {
+    await resetStubs()
+  })
+
+  test('User can view the availability checker', async ({ page }) => {
+    await login(page, { name: 'A TestUser' }, '/availability-checker?date=2026-07-27')
+
+    const availabilityCheckerPage = await AvailabilityCheckerPage.verifyOnPage(page)
+    await availabilityCheckerPage.assertSessionSelected(Session.MORNING)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', monday, 8)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', monday, 9)
+    await availabilityCheckerPage.assertRoomPartiallyAvailable('vvc-room-1', monday, '10:30', '11:00')
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', monday, 11)
+    await availabilityCheckerPage.assertRoomPartiallyAvailable('vvc-room-1', monday, '12:15', '12:30')
+    await availabilityCheckerPage.assertRoomPartiallyAvailable('vvc-room-1', monday, '12:45', '13:00')
+  })
+
+  test('User can view each tab on the availability checker', async ({ page }) => {
+    await login(page, { name: 'A TestUser' }, '/availability-checker?date=2026-07-27&period=AM')
+
+    const availabilityCheckerPage = await AvailabilityCheckerPage.verifyOnPage(page)
+    await availabilityCheckerPage.assertSessionSelected(Session.MORNING)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.MONDAY)
+
+    await availabilityCheckerPage.selectWeekDayTab(tuesday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.TUESDAY)
+    await availabilityCheckerPage.assertRoomPartiallyAvailable('vvc-room-1', tuesday, '08:00', '08:40')
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', tuesday, 9)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', tuesday, 10)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', tuesday, 11)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', tuesday, 12)
+
+    await availabilityCheckerPage.selectWeekDayTab(wednesday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.WEDNESDAY)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', wednesday, 8)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', wednesday, 9)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', wednesday, 10)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', wednesday, 11)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', wednesday, 12)
+
+    await availabilityCheckerPage.selectWeekDayTab(thursday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.THURSDAY)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', thursday, 8)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', thursday, 9)
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', thursday, 10)
+    await availabilityCheckerPage.assertRoomPartiallyAvailable('vvc-room-1', thursday, '11:00', '11:30')
+    await availabilityCheckerPage.assertRoomAvailable('vvc-room-1', thursday, 12)
+
+    await availabilityCheckerPage.selectWeekDayTab(friday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.FRIDAY)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', friday, 8)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', friday, 9)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', friday, 10)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', friday, 11)
+    await availabilityCheckerPage.assertRoomBooked('vvc-room-1', friday, 12)
+  })
+
+  test('User can change the date and session on the availability checker', async ({ page }) => {
+    await login(page, { name: 'A TestUser' }, '/availability-checker?date=2026-07-27&period=AM')
+
+    const availabilityCheckerPage = await AvailabilityCheckerPage.verifyOnPage(page)
+    await availabilityCheckerPage.assertSessionSelected(Session.MORNING)
+
+    await bookAVideoLinkApi.stubGetVideoLinkEvents({
+      monday: previousMonday(monday),
+      tuesday: previousTuesday(tuesday),
+      wednesday: previousWednesday(wednesday),
+      thursday: previousThursday(thursday),
+      friday: previousFriday(friday),
+    })
+
+    await availabilityCheckerPage.selectDate(previousMonday(monday))
+    await availabilityCheckerPage.selectSession(Session.AFTERNOON)
+    await availabilityCheckerPage.update()
+
+    await availabilityCheckerPage.assertSessionSelected(Session.AFTERNOON)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.MONDAY)
+  })
+
+  test('User can view previous week and next week on the availability checker', async ({ page }) => {
+    await login(page, { name: 'A TestUser' }, '/availability-checker?date=2026-07-27&period=AM')
+
+    const availabilityCheckerPage = await AvailabilityCheckerPage.verifyOnPage(page)
+    await availabilityCheckerPage.assertSessionSelected(Session.MORNING)
+
+    await bookAVideoLinkApi.stubGetVideoLinkEvents({
+      monday: previousMonday(monday),
+      tuesday: previousTuesday(tuesday),
+      wednesday: previousWednesday(wednesday),
+      thursday: previousThursday(thursday),
+      friday: previousFriday(friday),
+    })
+    await availabilityCheckerPage.previousWeek()
+
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.MONDAY)
+    await availabilityCheckerPage.selectWeekDayTab(previousTuesday(tuesday))
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.TUESDAY)
+    await availabilityCheckerPage.selectWeekDayTab(previousWednesday(wednesday))
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.WEDNESDAY)
+    await availabilityCheckerPage.selectWeekDayTab(previousThursday(thursday))
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.THURSDAY)
+    await availabilityCheckerPage.selectWeekDayTab(previousFriday(friday))
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.FRIDAY)
+
+    await bookAVideoLinkApi.stubGetVideoLinkEvents({
+      monday,
+      tuesday,
+      wednesday,
+      thursday,
+      friday,
+    })
+
+    await availabilityCheckerPage.nextWeek()
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.MONDAY)
+    await availabilityCheckerPage.selectWeekDayTab(tuesday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.TUESDAY)
+    await availabilityCheckerPage.selectWeekDayTab(wednesday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.WEDNESDAY)
+    await availabilityCheckerPage.selectWeekDayTab(thursday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.THURSDAY)
+    await availabilityCheckerPage.selectWeekDayTab(friday)
+    await availabilityCheckerPage.assertWeekDayTabSelected(WeekDay.FRIDAY)
+  })
+})
